c9b6d0568cba2d2065eeb0943ae2c265a9f7bc7c
Matthew Johnson <matthew.johnson@asynchrony.com>
2018-07-11 19:48:35 -0500

    adds a style sheet

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Header from './components/header';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import Header from './components/header'
 
 export default class App extends Component {
   render() {
-    return (<div>
-      <Header />
-      <p>Now we're cooking!!!</p>
-    </div>);
+    return (
+      <div>
+        <Header />
+        <p>Now we're cooking!!!</p>
+      </div>
+    )
   }
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (<div>
-      <h2>Clarke Tech Local</h2>
+      <h2>CT Local</h2>
     </div>);
   }
 }
