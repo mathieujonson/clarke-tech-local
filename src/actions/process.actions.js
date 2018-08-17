af60467cf81807bdf2257c30f1a74a60e662696c
Matthew Johnson <matthew.johnson@asynchrony.com>
2018-08-17 16:49:42 -0400

    saves added processes to the electron store

--- src/actions/process.actions.js
+++ src/actions/process.actions.js
@@ -1,5 +1,12 @@
 import * as types from './actionTypes'
 
+const Store = require('electron-store')
+const store = new Store()
+
 export function saveProcess(json) {
+  store.set(`processes.${json.name}`, {
+    command: json.command,
+    directory: json.directory
+  })
   return { type: types.SAVE_PROCESS, payload: json }
 }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,8 +5,12 @@ export default class List extends Component {
   render() {
     return (
       <footer>
-        <Link to="/"><i className="material-icons">list</i>List</Link>
-        <Link to="/add"><i className="material-icons">add</i>Add</Link>
+        <Link to="/">
+          <i className="material-icons">list</i>List
+        </Link>
+        <Link to="/add">
+          <i className="material-icons">add</i>Add
+        </Link>
       </footer>
     )
   }
